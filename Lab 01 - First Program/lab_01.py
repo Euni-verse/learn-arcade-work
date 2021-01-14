90cedeeae23cf42112c668f2e771cb9243963551
Euni-verse <eunicemariap@gmail.com>
2021-01-14 15:32:32 -0800

    initial changes to lab one

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,26 @@
 print("hi verse")
+print("Mes yeux ont été créés pour voir le ciel")
+print("Il y a des jours ou mes yieux ne voient pas le soleil")
+
+print("In typography, there is a difference between qutoation marks \" and inch marks")
+print("\"The rain in spain fall hardly in the plain\" said my fair lady")
+
+print("Linefeed is to move a cursor down, so for instance\nevery\nword\nhence\nfourth\nwill\nbe\non\nthe\nnext\nline")
+print("doineedspacesbetweenwords?")
+print("apparently\nI\ndo")
+print("Don't put spaces around escape codes unless you want them there")
+print("For instance \n I \n want \n these \n words \n to \n line \n up")
+print("But they don't really line up really nicely")
+print("""You can
+also print on
+multiple lines
+using triple
+quotations.
+This basically
+allows the same
+soft return option
+without the need to
+use the backslash n
+to bring the next
+word to the next
+line""")
