8f08497f4a3226063a50682514cfb4c5818d0406
Euni-verse <eunicemariap@gmail.com>
2021-01-17 15:06:32 -0800

    drawing window

---
+++ Scratch Folder/drawings_samples.py
@@ -0,0 +1,23 @@
+""" This is a drawing
+"""
+
+# import arcade library
+import arcade
+
+# open a drawing window
+arcade.open_window(600, 600, "Drawing Example")
+
+# set the background color
+arcade.set_background_color(arcade.color.BITTERSWEET)
+
+# get ready to Drawing
+arcade.start_render()
+
+# drawing code goes here
+
+
+# finish Drawing
+arcade.finish_render()
+
+# keep the drawing window open
+arcade.run()
